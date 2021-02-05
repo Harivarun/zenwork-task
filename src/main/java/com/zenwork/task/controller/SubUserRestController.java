56d7cc2d8bac6fc1c83fba85852eabb2ddda784b
Harivarun <mshvarunkumar@gmail.com>
2021-02-06 02:18:13 +0530

    Added CRUD operations for Subuser

--- src/main/java/com/zenwork/task/controller/SubUserRestController.java
+++ src/main/java/com/zenwork/task/controller/SubUserRestController.java
@@ -4,16 +4,16 @@ import com.codahale.metrics.annotation.Timed;
 import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.DeleteResult;
 import com.zenwork.task.dao.Subuser;
 import com.zenwork.task.service.SubuserService;
 import org.bson.Document;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
 import javax.validation.Validator;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
@@ -34,6 +34,12 @@ public class SubUserRestController {
         this.validator = validator;
         this.collection = collection;
         this.subuserService = subuserService;
+    }
+
+    @GET
+    @Path("/getAll")
+    public Response getAllSubusers(){
+        return Response.ok(subuserService.getSubusers(collection)).build();
     }
 
     @POST
@@ -75,10 +81,46 @@ public class SubUserRestController {
             String json = gson.toJson(subuser);
             subuserService.createSubuser(collection, new Document(BasicDBObject.parse(json)));
             Map<String, String> response = new HashMap<>();
-            response.put("message", "SubUser created successfully");
+            response.put("message", "Subuser created successfully");
             return Response.ok(response).build();
         }
     }
 
+    @PUT
+    @Path("/update")
+    @Timed
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateSubuser(@NotNull @Valid final Subuser subuser) {
 
+        Set<ConstraintViolation<Subuser>> violations = validator.validate(subuser);
+        if(violations.size() > 0) {
+            ArrayList<String> validationMessages = new ArrayList<>();
+            for (ConstraintViolation<Subuser> violation : violations) {
+                validationMessages.add(violation.getPropertyPath().toString() + ": " + violation.getMessage());
+            }
+            return Response.status(Response.Status.BAD_REQUEST).entity(validationMessages).build();
+        }else{
+            subuserService.updateSubuser(collection, subuser);
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Subuser updated successfully");
+            return Response.ok(response).build();
+        }
+    }
+
+    @DELETE
+    @Timed
+    @Path("/delete/{email}")
+    public Response deleteSubuser(@PathParam("email") final String email) {
+        DeleteResult result = subuserService.deleteSubuser(collection, email);
+        Map<String, String> response = new HashMap<>();
+        Document doc = subuserService.findSubuser(collection,email);
+        if(doc == null){
+            response.put("message", "Subuser with email: " + email + " deleted successfully");
+            return Response.ok(response).build();
+        }else {
+            response.put("message","Subuser not found");
+            return  Response.status(Response.Status.BAD_REQUEST).entity(response).build();
+        }
+    }
 }
--- src/main/java/com/zenwork/task/service/SubuserService.java
+++ src/main/java/com/zenwork/task/service/SubuserService.java
@@ -1,9 +1,18 @@
 package com.zenwork.task.service;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import com.zenwork.task.dao.Subuser;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.combine;
+import static com.mongodb.client.model.Updates.set;
 
 public class SubuserService {
 
@@ -14,5 +23,23 @@ public class SubuserService {
 
     public Document findSubuser(MongoCollection<Document> collection, String email) {
         return collection.find(eq("email",email)).first();
+    }
+
+    public void updateSubuser(MongoCollection<Document> collection, Subuser subuser) {
+        Bson filter = eq("email", subuser.getEmail());
+        Bson nameField = set("name", subuser.getName());
+        Bson passwordField = set("password",subuser.getPassword());
+        Bson storeID = set("storeID",subuser.getStoreID());
+        Bson subuserDoc = combine(nameField,passwordField,storeID);
+
+        UpdateResult updateResult = collection.updateOne(filter, subuserDoc);
+    }
+
+    public List<Document> getSubusers(MongoCollection<Document> collection) {
+        return collection.find().into(new ArrayList<>());
+    }
+
+    public DeleteResult deleteSubuser(MongoCollection<Document> collection, String email) {
+       return collection.deleteOne(eq("email",email));
     }
 }
